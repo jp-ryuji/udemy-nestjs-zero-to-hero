68f1cc2b65689c0fa0555ce57e6a09987a3c64f4
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-19 00:45:57 +0900

    [50] nest g module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule],
+    TasksModule,
+    AuthModule],
 })
 export class AppModule {}
