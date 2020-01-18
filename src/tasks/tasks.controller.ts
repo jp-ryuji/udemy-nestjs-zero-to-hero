6e593b2e006ea3a2a5bf3903198175e2fe8063ff
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-18 23:41:53 +0900

    [47, 48] refactor getTasks using repository

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -13,14 +13,10 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  // @Get()
-  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-  //   if (Object.keys(filterDto).length) {
-  //     return this.tasksService.getTasksWithFilters(filterDto);
-  //   } else {
-  //     return this.tasksService.getAllTasks();
-  //   }
-  // }
+  @Get()
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto) {
+    return this.tasksService.getTasks(filterDto);
+  }
 
   @Get('/:id')
   getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
