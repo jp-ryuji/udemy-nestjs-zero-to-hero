9bc719d360ee9371ee7baf7d43668d1a56494693
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-18 23:24:23 +0900

    [45] refactor deleteTask using repository

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -32,10 +32,10 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  // @Delete('/:id')
-  // deleteTask(@Param('id') id: string) {
-  //   this.tasksService.deleteTask(id);
-  // }
+  @Delete('/:id')
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
+  }
 
   // @Patch('/:id/status')
   // updateTaskStatus(
