ddb63c1284d70a567bb5986d6ec1ca70c851df06
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-20 06:37:46 +0900

    small cosmetic fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     TasksModule,
-    AuthModule],
+    AuthModule,
+  ],
 })
 export class AppModule {}
