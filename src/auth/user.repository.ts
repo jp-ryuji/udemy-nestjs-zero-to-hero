5928639818c9c208932cbaaaed6e051544e6cafe
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-19 12:48:23 +0900

    [54] hash passwords using salts

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import { EntityRepository, Repository } from 'typeorm';
 
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
@@ -12,7 +13,9 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
     try {
       await user.save();
     } catch (error) {
@@ -22,5 +25,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
