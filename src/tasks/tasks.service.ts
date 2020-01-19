34789da4a3aea1507957da0992d3a023fb8f6911
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-19 16:08:05 +0900

    [65] get a user's task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -23,8 +23,11 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(
+    id: number,
+    user: User,
+  ): Promise<Task> {
+    const found = await this.taskRepository.findOne({ where: { id, userId: user.id }});
 
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -48,10 +51,10 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
-    task.status = status;
-    await task.save();
-    return task;
-  }
+  // async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+  //   const task = await this.getTaskById(id);
+  //   task.status = status;
+  //   await task.save();
+  //   return task;
+  // }
 }
