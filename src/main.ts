3f18f64a9fa387d9f398ee4ffe5cdb02c16421fb
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-19 17:27:00 +0900

    [76] enable cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,10 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors(); // NOTE: this accepts all incominng requests.
+  }
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
