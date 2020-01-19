230dd81c8a2c0a500e5c3bef6149b470fe1cc0ac
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-19 18:02:20 +0900

    [80] replace bcrypt with bcryptjs

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Task } from 'src/tasks/task.entity';
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
