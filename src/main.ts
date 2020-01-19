0ff90a45552ef653ab15bda7258cc42f8035298c
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2020-01-20 07:01:26 +0900

    rewrite main.ts to comply with fastify

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import * as config from 'config';
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const serverConfig: any = config.get('server'); // NOTE: use `any` for now.
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors(); // NOTE: this accepts all incominng requests.
